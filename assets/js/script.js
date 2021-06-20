083333f7dbd3d799690e268666beeb6fb53612b4
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-06-20 12:47:17 +0700

    add interface, handle item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 class monHoc {
-    constructor(maMonHoc, tenMonHoc, bf, at, level, tinChi){
+    constructor(id, maMonHoc, tenMonHoc, bf, at, level, tinChi, batBuoc){
         this.maMonHoc = maMonHoc;
         this.tenMonHoc = tenMonHoc;
         this.bf = [...bf];
         this.at = [...at];
         this.level = level;
         this.tinChi = tinChi;
+        this.id = id;
+        this.batBuoc = batBuoc;
     }
     getTen() {
         return this.tenMonHoc;
@@ -25,8 +27,27 @@ class monHoc {
     getTinChi() {
         return this.tinChi;
     }
+    isBatBuoc() {
+        return this.batBuoc;
+    }
 }
-const maMH = ["QP006","QP007","QP008","QP009","TC100","XH023","XH024","XH025","XH031","XH032","XH033","FL001","FL002","FL003","FL007","FL008","FL009","ML014","ML016","ML018","ML019","ML021","KL001","ML007","XH028","XH011","XH012","XH014","KN001","KN002","TN001","TN002","TN010","TN012","CT100","CT200"];
+const maMH = ["QP006","QP007","QP008","QP009","TC100","XH023","XH024","XH025","XH031","XH032","XH033","FL001","FL002","FL003","FL007","FL008","FL009","ML014","ML016","ML018","ML019","ML021","KL001","ML007","XH028","XH011","XH012","XH014","KN001","KN002","TN001","TN002","TN010","TN012","CT100","CT200",
+    "CT172",
+    "CT101",
+    "CT177",
+    "CT175",
+    "CT174",
+    "CT180",
+    "CT173",
+    "CT178",
+    "CT112",
+    "CT176",
+    "CT296",
+    "CT182",
+    "CT179",
+    "CT188",
+    "CT190"
+];
 const tenMH = ["Giáo dục quốc phòng và An ninh 1",
   "Giáo dục quốc phòng và An ninh 2",
   "Giáo dục quốc phòng và An ninh 3",
@@ -62,8 +83,73 @@ const tenMH = ["Giáo dục quốc phòng và An ninh 1",
   "Xác suất thống kê",
   "Đại số tuyến tính và hình học",
   "Kỹ năng học đại học",
-  "Nền tảng công nghệ thông tin"
+  "Nền tảng công nghệ thông tin",
+
+  "Toán rời rạc",
+  "Lập trình căn bản A",
+  "Cấu trúc dữ liệu",
+  "Lý thuyết đồ thị",
+  "Phân tích và thiết kế thuật toán",
+  "Cơ sở dữ liệu",
+  "Kiến trúc máy tính",
+  "Nguyên lý hệ điều hành",
+  "Mạng máy tính",
+  "Lập trình hướng đối tượng",
+  "Phân tích và thiết kế hệ thống",
+  "Ngôn ngữ mô hình hóa",
+  "Quản trị hệ thống",
+  "Nhập môn lập trình Web",
+  "Nhập môn trí tuệ nhân tạo"
 
 ];
-console.log(maMH);
-console.log(tenMH);
+const batBuoc = [
+    1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,
+    0,0,0,0,0,0,0,
+    1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+];
+
+let addToAccordion = (id, from, to) => {
+    const accordion_dc = document.querySelector('#'+id);
+    for(let i=from;i<to;i++){
+        let inf;
+        let subject = new monHoc(i+1, maMH[i], tenMH[i], ['no'], ['no'], 3, 3, batBuoc[i]);
+        let temp = document.createElement('li');
+        temp.setAttribute('class','list-group-item');
+
+        let temp2 = document.createElement('div');
+        temp2.setAttribute('class','row');
+
+        // ma mon hoc
+        let ma = document.createElement('div');
+        ma.setAttribute('class','col-2');
+        inf = document.createTextNode(subject.getMa());
+        ma.appendChild(inf);
+
+        // ten mon hoc
+        let ten = document.createElement('div');
+        ten.setAttribute('class','col-4');
+        inf = document.createTextNode(subject.getTen());
+        ten.appendChild(inf);
+
+        // bat buoc
+        let bb = document.createElement('div');
+        bb.setAttribute('class','col-1');
+        inf = document.createTextNode('Bắt buộc');
+        if(subject.isBatBuoc()) bb.appendChild(inf);
+
+        
+        temp2.appendChild(ma);
+        temp2.appendChild(ten);
+        temp2.appendChild(bb);
+
+        temp.appendChild(temp2);
+        accordion_dc.appendChild(temp);
+    }
+}
+
+
+addToAccordion('accordion_dc',0,36);
+addToAccordion('accordion_csn',36,51);

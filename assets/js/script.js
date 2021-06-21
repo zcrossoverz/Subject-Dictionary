7b2615238277762d8a0c8fa889c72195c88da547
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-06-21 13:27:01 +0700

    add modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,9 @@ class monHoc {
     isBatBuoc() {
         return this.batBuoc;
     }
+    getId(){
+        return this.id;
+    }
 }
 const maMH = ["QP006","QP007","QP008","QP009","TC100","XH023","XH024","XH025","XH031","XH032","XH033","FL001","FL002","FL003","FL007","FL008","FL009","ML014","ML016","ML018","ML019","ML021","KL001","ML007","XH028","XH011","XH012","XH014","KN001","KN002","TN001","TN002","TN010","TN012","CT100","CT200",
     "CT172",
@@ -124,22 +127,89 @@ let addToAccordion = (id, from, to) => {
 
         // ma mon hoc
         let ma = document.createElement('div');
-        ma.setAttribute('class','col-2');
+        ma.setAttribute('class','col-3');
         inf = document.createTextNode(subject.getMa());
         ma.appendChild(inf);
 
         // ten mon hoc
         let ten = document.createElement('div');
-        ten.setAttribute('class','col-4');
+        ten.setAttribute('class','col-5');
         inf = document.createTextNode(subject.getTen());
         ten.appendChild(inf);
 
-        // bat buoc
+        // modal
         let bb = document.createElement('div');
-        bb.setAttribute('class','col-1');
-        inf = document.createTextNode('Bắt buộc');
-        if(subject.isBatBuoc()) bb.appendChild(inf);
+        bb.setAttribute('class','col');
+        let btnModal = document.createElement('button');
+        btnModal.setAttribute('class','btn btn-primary');
+        btnModal.setAttribute('data-bs-toggle','modal');
+        btnModal.setAttribute('data-bs-target','#modal_'+subject.getId());
+        btnModal.appendChild(document.createTextNode('Xem thêm'));
+        bb.appendChild(btnModal);
 
+            //// header
+            let modalHeader = document.createElement('div');
+            modalHeader.setAttribute('class','modal-header');
+            let modalTitle = document.createElement('h5');
+            modalTitle.setAttribute('class','modal-title');
+            modalTitle.setAttribute('id','staticBackdropLabel');
+            modalTitle.appendChild(document.createTextNode(subject.getTen()));
+
+            let modalBtnClose = document.createElement('div');
+            modalBtnClose.setAttribute('class','btn-close');
+            modalBtnClose.setAttribute('data-bs-dismiss','modal');
+            modalBtnClose.setAttribute('aria-label','Close');
+
+            modalHeader.appendChild(modalTitle);
+            modalHeader.appendChild(modalBtnClose);
+
+            //// end header
+
+            //// body
+            let modalBody = document.createElement('div');
+
+            modalBody.setAttribute('class','modal-body');
+            modalBody.appendChild(document.createTextNode('test'));
+
+
+            //// end body
+
+
+            //// footer
+            let modalFooter = document.createElement('div');
+            modalFooter.setAttribute('class','modal-footer')
+            let footerCloseBtn = document.createElement('button');
+            footerCloseBtn.setAttribute('class','btn btn-secondary');
+            footerCloseBtn.setAttribute('data-bs-dismiss','modal');
+            footerCloseBtn.appendChild(document.createTextNode('Đóng'));
+            modalFooter.appendChild(footerCloseBtn);
+
+
+            //// end footer
+
+            let modalContent = document.createElement('div');
+            modalContent.setAttribute('class','modal-content');
+            modalContent.appendChild(modalHeader);
+            modalContent.appendChild(modalBody);
+            modalContent.appendChild(modalFooter);
+
+            let modalDialog = document.createElement('div');
+            modalDialog.setAttribute('class','modal-dialog');
+            modalDialog.appendChild(modalContent);
+
+
+            let modalDiv = document.createElement('div');
+            modalDiv.setAttribute('class','modal fade');
+            modalDiv.setAttribute('id','modal_'+subject.getId());
+            modalDiv.setAttribute('data-bs-backdrop','static');
+            modalDiv.setAttribute('data-bs-keyboard','false');
+            modalDiv.setAttribute('tabindex','-1');
+            modalDiv.setAttribute('aria-labelledby','staticBackdropLabel');
+            modalDiv.setAttribute('aria-hidden','true');
+
+            modalDiv.appendChild(modalDialog);
+
+        bb.appendChild(modalDiv);
         
         temp2.appendChild(ma);
         temp2.appendChild(ten);
